9e4ed2e492f9fbb6d27d4b070a385e798024c367
udayrastogi0531 <udayprakashrastogi2005@gmail.com>
2025-07-03 04:39:26 +0530

    Initial commit for LabubuN

---
+++ script.js
@@ -0,0 +1,8 @@
+const slides = document.querySelectorAll('.character');
+let index = 0;
+
+setInterval(() => {
+  slides[index].classList.remove('active');
+  index = (index + 1) % slides.length;
+  slides[index].classList.add('active');
+}, 3000);
